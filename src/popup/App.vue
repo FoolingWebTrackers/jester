b2a1f9033d51b805b6e52eb4014c1fdbc5f979c7
MeteYokus2402 <myokus21@ku.edu.tr>
2024-11-04 19:24:24 +0300

    Base UI initialized

--- src/popup/App.vue
+++ src/popup/App.vue
@@ -1,12 +1,29 @@
 <template>
-  <div class="mainPage-container">
-    <input type="checkbox" v-model="isChecked" id="checkbox" />
-    <label for="checkbox"
-      >Open Google (when checked) / YouTube (when unchecked)</label
-    >
-    <label v-if="isChecked">Denem Denemeadwad</label>
-    <button @click="openSite">Open Site</button>
+  <div class="mainPage-header">
+    <img :src="require('@/assets/icon-128.png')" alt="Icon" class="icon" />
   </div>
+  <div class="mainPage-container">
+    <div class="button-container">
+      <div class="button-group">
+        <button class="icon-button"></button>
+        <label>Create</label>
+        <label style="margin-top: -3px">Personas</label>
+      </div>
+      <div class="button-group">
+        <button class="icon-button"></button>
+        <label>Fake Mic</label>
+      </div>
+      <div class="button-group">
+        <button class="icon-button"></button>
+        <label>Profile</label>
+      </div>
+      <div class="button-group">
+        <button class="icon-button"></button>
+        <label>Settings</label>
+      </div>
+    </div>
+  </div>
+  <div class="mainPage-footer">Jester</div>
 </template>
 
 <script>
@@ -15,6 +32,7 @@ export default {
   data() {
     return {
       isChecked: false, // Default unchecked
+      iconSrc: "../../public/icon-128.png",
     };
   },
   methods: {
@@ -29,22 +47,67 @@ export default {
 </script>
 
 <style scoped>
-button {
-  background-color: #c71b15;
-  color: white;
-  padding: 10px;
+.icon {
+  width: 45px; /* Adjust the size as needed */
+  height: auto; /* Maintain aspect ratio */
+}
+.icon-button {
+  width: 50px;
+  height: 50px;
+  background-color: #4a0000;
+  border-radius: 50%;
   border: none;
   cursor: pointer;
+}
+.button-container {
+  align-items: baseline;
+  display: flex;
+  margin-top: 20px;
+  width: 100%;
+  justify-content: space-evenly;
+  height: 100px; /* Adjusted height to accommodate labels */
+}
+.button-group {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+}
+
+label {
+  margin-top: 5px; /* Space between button and label */
+  color: #d1d1d1; /* Adjust label color as needed */
 }
 
 input {
   margin-right: 10px;
 }
+.mainPage-footer {
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 30px;
+  background: #383636;
+  box-shadow: 0px -10px 20px rgba(0, 0, 0, 0.5);
+  color: #761626;
+  font-size: 15pt;
+}
+.mainPage-header {
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #383636;
+  height: 70px;
+  box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.5);
+}
 .mainPage-container {
   display: flex;
-  width: 450px;
-  height: 750px;
-  background: #383636;
+  width: 300px;
+  height: 500px;
+  background: #1e1e1e;
   margin: 0 px;
   padding: 0px;
 }
@@ -53,7 +116,8 @@ input {
 body {
   overflow: hidden; /* Hide scrollbars */
   margin: 0px;
-  width: 450px;
+  width: 300px;
   height: 600px;
+  background: transparent;
 }
 </style>
